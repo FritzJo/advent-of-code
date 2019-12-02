650d918e91546544bf1cb62e31b0c11b42b666c0
FritzJo <fritzjo-git@mailbox.org>
2019-12-02 11:31:39 +0100

    added day1 / puzzle 1

---
+++ 2019/Day 1/AoC_Day_1_Puzzle_1.py
@@ -0,0 +1,23 @@
+# Advent of Code 2019
+# Day 1 - Puzzle: 1
+
+def calculate_fuel(mass):
+	return int(mass / 3) - 2
+	
+def read_input(filename):
+	file = open(filename)
+	values = []
+	for line in file:
+		values.append(int(line))
+	return values
+	
+def calculate_total_fuel(masses):
+	sum = 0
+	for mass in masses:
+		sum += calculate_fuel(mass)
+	return sum
+	
+if __name__ == "__main__":
+	masses = read_input("AoC_Day_1_Puzzle_1_Input.txt")
+	total_fuel = calculate_total_fuel(masses)
+	print(total_fuel)
