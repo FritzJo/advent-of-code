087152385db338cb7a985ff1d2a20b99477987c0
FritzJo <fritzjo-git@mailbox.org>
2020-12-03 10:41:16 +0100

    First puzzle of day 3

---
+++ 2020/Day 3/Puzzle_1.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func readInput(filename string) [][]string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines [][]string
+	for scanner.Scan() {
+		row := scanner.Text()
+		var rowSlice []string
+		for _, field := range row {
+			rowSlice = append(rowSlice, string(field))
+		}
+		lines = append(lines, rowSlice)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
+
+func calculatePath(pattern [][]string, down int, right int) int {
+	i := 0
+	j := 0
+	trees := 0
+
+	for i < len(pattern) {
+		if pattern[i][j] == "#" {
+			trees++
+		}
+		i = i + down
+		j = (j + right) % len(pattern[0])
+	}
+	return trees
+}
+
+func part1() int {
+	pattern := readInput("input2.txt")
+	return calculatePath(pattern, 1, 3)
+}
